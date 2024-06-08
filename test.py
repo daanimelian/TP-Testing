cbb0455c8354f7771efb7534d0cdfd9720056d82
Daniela Melian <meliandaani@gmail.com>
2024-06-07 21:45:56 -0300

    test de prueba

---
+++ test.py
@@ -0,0 +1,20 @@
+# Pensar al menos 5 casos de prueba
+# Automatizar al menos 4
+# Armar una ppt con el proceso del tpo:
+# 1. Crear una presentación en la que deberán especificar:
+# o Integrantes del Equipo
+# o Plataforma seleccionada para llevar a cabo la automatización (aplicación web, o
+# aplicación local)
+# § ¿Por qué eligieron esta aplicación?
+# o Presentación de los Casos de Prueba creados:
+# § ¿Cuáles son las funcionalidades de la aplicación que cubren los casos de
+# prueba?
+# § ¿Por qué seleccionaron ésta / éstas?
+# § ¿Identificaron otras funcionalidades para las que no crearon casos de prueba?
+# § ¿Utilizaron alguna técnica para identificar o diagramar las funcionalidades de la
+# aplicación? (por ejemplo, transición de estados o diagramas de actividad)
+# • De ser así, presenten los diagramas correspondientes
+# o Sobre los Casos de Prueba automatizados:
+# § Justificación de la selección de los Casos de Prueba que automatizarán
+# • ¿Prioridad, facilidad de la automatización u otros criterios?
+# • Indiquen el orden en que automatizaron los casos de prueba
